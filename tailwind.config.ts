ff67c33567a5ea0125b3368a80c06c656e791e75
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-12 23:01:29 +0900

    💄 다크모드 추가
    - DarkmodeToggle.tsx 추가
    - global.css 반전 컬러 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -54,5 +55,4 @@ export default {
     },
   },
   plugins: [],
-  darkMode: 'media',
 } satisfies Config;
