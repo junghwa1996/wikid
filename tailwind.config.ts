8cdcfb03b1a30db3738fcc703079272c57b27206
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-12 16:15:46 +0900

    💄 컬러변수 설정 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,31 @@ export default {
     extend: {
       colors: {
         background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        white: 'var(--white)',
+        gray: {
+          100: 'var(--gray-100)',
+          200: 'var(--gray-200)',
+          300: 'var(--gray-300)',
+          400: 'var(--gray-400)',
+          500: 'var(--gray-500)',
+          600: 'var(--gray-600)',
+        },
+        green: {
+          100: 'var(--green-100)',
+          200: 'var(--green-200)',
+          300: 'var(--green-300)',
+        },
+        red: {
+          50: 'var(--red-50)',
+          100: 'var(--red-100)',
+          200: 'var(--red-200)',
+        },
+        purple: {
+          100: 'var(--purple-100)',
+        },
+        yellow: {
+          100: 'var(--yellow-100)',
+        },
       },
     },
   },
