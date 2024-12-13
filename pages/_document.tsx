89e49fbbd2219811f9440c3948b2b7c975751319
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-13 16:09:26 +0900

    🔨 eslint에 맞게 자동 수정된 파일

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="kr">
       <Head>
         <link
-          rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+          rel="stylesheet"
         />
         <title>wikid</title>
       </Head>
