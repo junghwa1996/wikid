12aef074fe373547e0c85b866a5c1ff205dcd7a4
김예도 <kyd629@naver.com>
2024-12-16 23:50:02 +0900

    🐛 ESLint에 맞게 수정

---
+++ components/GNB/GNB.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
+
+import Alarm from './Alarm';
+import Header from './Header';
+import Login from './Login';
+import ProfileIcon from './ProfileIcon';
+
+export default function GNB() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // ANCHOR TODO
+  const [isMobile, setIsMobile] = useState(false);
+  const profileMenuRef = useRef<HTMLDivElement>(null);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    const checkMobile = () => setIsMobile(window.innerWidth <= 767);
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={profileMenuRef}
+      className="z-100 fixed left-0 top-0 flex h-20 w-full items-center justify-between bg-background px-20 py-5 shadow-custom"
+    >
+      <Header />
+
+      {isLoggedIn ? (
+        <div className="flex items-center gap-5">
+          <Alarm />
+          <ProfileIcon isMobile={isMobile} logout={handleLogout} />
+        </div>
+      ) : (
+        <Login login={handleLogin} isMobile={isMobile} />
+      )}
+    </div>
+  );
+}
