1bdee5f617b6f38f6227766a3e9ae1c1c3471dbb
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-17 23:06:23 +0900

    🔨 image가 없을때 노출되는 아이콘 추가
    - 직접 아이콘 만들었습니다 ㅎ

--- pages/boards/components/BoardCard.tsx
+++ pages/boards/components/BoardCard.tsx
@@ -24,10 +24,14 @@ export default function BoardCard({
   return (
     <Link
       href={`/boards/${id}`}
-      className="block h-auto w-[250px] overflow-hidden rounded-custom shadow-custom ta:w-[302px]"
+      className="block h-auto w-[250px] overflow-hidden rounded-custom shadow-custom dark:shadow-custom-dark ta:w-[302px]"
     >
-      <div className="relative h-[130px] w-auto overflow-hidden">
-        <Image src={image} alt="베스트 게시글 이미지" fill />
+      <div className="relative h-[130px] w-auto overflow-hidden bg-gray-100">
+        <Image
+          src={image === '' ? '/icon/icon-no-image.svg' : image}
+          alt="베스트 게시글 이미지"
+          fill
+        />
       </div>
       <div className="bg-background px-[20px] pb-[14px] pt-[20px] mo:pt-[11px]">
         <h3 className="mb-[6px] text-18sb mo:mb-0 mo:text-16sb">{title}</h3>
