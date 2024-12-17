fafa373501ff0164f8de6cd3830c51c2522faeac
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-17 16:59:38 +0900

    ✨ Heart 카운팅 컴포넌트
    - 호출 시 onClick이 있으면 컬러 변경
    - 없을 경우 클릭 없이 정적 요소로 사용

---
+++ components/Heart/Heart.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+import HeartIcon from './HeartIcon';
+
+interface HeartProps {
+  initialCount: number;
+  onClick?: () => void;
+}
+
+/**
+ * Heart count component
+ * @param {number} initialCount - 초기 카운트 수
+ * @param {function} onClick - 클릭 시 동작 할 이벤트(옵션)
+ * @example <Heart initialCount={10} onClick={() => console.log('클릭')} />
+ */
+export default function Heart({ initialCount, onClick }: HeartProps) {
+  const [isClicked, setIsClicked] = useState(false);
+  const [count, setCount] = useState(initialCount);
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+      setIsClicked((prev) => !prev);
+      setCount((prevCount) => (isClicked ? prevCount - 1 : prevCount + 1));
+    }
+  };
+
+  const clickStyles = {
+    icon: isClicked ? 'var(--red-100)' : 'var(--gray-400)',
+    text: isClicked && 'text-red-100',
+  };
+  return (
+    <div
+      className="flex cursor-pointer items-center gap-1"
+      onClick={handleClick}
+    >
+      <HeartIcon fill={clickStyles.icon} />
+      <span className={`text-14 text-gray-400 mo:text-12 ${clickStyles.text}`}>
+        {count}
+      </span>
+    </div>
+  );
+}
