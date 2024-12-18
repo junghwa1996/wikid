6bc707074374d417a5d2436ed13d6c35c8a4595f
haksoo <haksoo0918@gmail.com>
2024-12-18 15:33:00 +0900

    🔨 날짜 및 주석 추가

--- pages/addboard.tsx
+++ pages/addboard.tsx
@@ -8,9 +8,16 @@ import TextEditor from '@/components/TextEditor';
 const MAX_TITLE = 30;
 
 // HTML에서 Text만 추출하는 함수
-function extractContent(str: string) {
-  return str.replace(/<[^>]*>/g, '').trim();
-}
+const extractContent = (str: string) => str.replace(/<[^>]*>/g, '').trim();
+
+// 포멧된 날짜 반환하는 함수
+const formatDate = (date: string) => {
+  const d = new Date(date);
+  const year = d.getFullYear();
+  const month = (d.getMonth() + 1).toString().padStart(2, '0');
+  const day = d.getDate().toString().padStart(2, '0');
+  return `${year}. ${month}. ${day}.`;
+};
 
 /**
  * 게시글 등록하기 페이지
@@ -19,20 +26,26 @@ export default function Addboard() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
+  // 등록 버튼 비활성화
   const submitDisabled = title.length === 0 || content.length === 0;
+  // 내용에서 추출된 텍스트
   const textContent = extractContent(content);
+  // 오늘 날짜
+  const today = formatDate(new Date().toISOString());
 
+  // 제목 input 콜백 함수
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     // console.log('handleChange:e:', e);
     setTitle(e.target.value);
   };
+  // 내용 에디터 콜백 함수
   const handleContentChange = (value: string) => {
     // console.log('value:', value);
     setContent(value);
   };
+  // 작성 폼 서브밋 함수
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('submit work!');
     // API 연결 및 처리
     // TODO - 로그인 및 인증되면 그때 다시 테스트
     instance
@@ -66,7 +79,8 @@ export default function Addboard() {
             </header>
 
             <div className="my-6 text-16 text-gray-300 mo:my-4 mo:text-12">
-              등록일<span className="ml-3 mo:ml-2">2000. 00. 00.</span>
+              등록일
+              <span className="ml-3 mo:ml-2">{today}</span>
             </div>
 
             <form
