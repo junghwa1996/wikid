b44067c622b1d548624e3adecc178bf59b0f583b
김예도 <kyd629@naver.com>
2024-12-18 17:38:40 +0900

    📝 파라미터 주석 추가

--- pages/test/wiki/ContentHeader.tsx
+++ pages/test/wiki/ContentHeader.tsx
@@ -8,6 +8,14 @@ interface ContentHeaderProps {
   isEditing: boolean;
 }
 
+/**
+ * Content 컴포넌트의 헤더에 해당하는 컴포넌트
+ * @param name 위키문서의 이름
+ * @param link 위키문서의 링크
+ * @param onNameChange 이름이 변경될 때 호출되는 콜백 함수
+ * @param isEditing 편집 모드인지 여부
+ */
+
 export default function ContentHeader({
   name,
   link,
