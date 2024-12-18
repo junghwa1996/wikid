1609fcfa631f728f4a5b5c201d0843ee1a73e101
haksoo <haksoo0918@gmail.com>
2024-12-18 15:45:49 +0900

    🔨 다크모드 버튼 위치 변경, 헤더 삽입

--- pages/addboard.tsx
+++ pages/addboard.tsx
@@ -2,6 +2,7 @@ import instance from 'lib/axios-client';
 import { FormEvent, useState } from 'react';
 
 import Button from '@/components/Button';
+import Headers from '@/components/Headers/Headers';
 import TextEditor from '@/components/TextEditor';
 
 // 제목 글자수 제한
@@ -60,10 +61,10 @@ export default function Addboard() {
 
   return (
     <div className="min-h-svh">
-      <header>GNB 영역</header>
+      <Headers />
 
       <main>
-        <div className="container">
+        <div className="container pt-20">
           <div className="mb-5 mt-[54px] rounded-custom px-[30px] py-[30px] shadow-custom dark:shadow-custom-dark">
             <header className="my-4 flex items-center justify-between">
               <h1 className="mo:text-16sb ta:text-20sb pc:text-24sb">
