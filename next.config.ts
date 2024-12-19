48a2cdae8fd3174c4343e57c1794272bffad04d5
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-19 18:24:19 +0900

    📦 react 버전다운, eslint 버전업
    - 안정성 있는 버전으로 설정

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: ['via.placeholder.com'], // 허용할 이미지 도메인 추가
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+    ],
   },
 };
 
