b52d7cbbbc30e75f24c5dd865267ca922875a618
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-20 14:49:27 +0900

    🧪 ✨ 임시 테스트 페이지로 사용 (추후 실 페이지로 사용)

---
+++ pages/boards/[articleId].tsx
@@ -0,0 +1,46 @@
+import BoardDetailCard from './components/BoardDetailCard';
+
+{
+  /* ANCHOR - 리뷰를 위해서 추가한 코드 */
+}
+const data = {
+  updatedAt: '2024-12-20T05:08:25.226Z',
+  createdAt: '2024-12-20T05:08:25.226Z',
+  likeCount: 0,
+  writer: {
+    name: '이름',
+    id: 1,
+  },
+  image: 'string',
+  title: '게시글 제목입니다.',
+  id: 1,
+  isLiked: true,
+  content: '게시글 내용입니다.',
+};
+
+export default function boardsDetails() {
+  return (
+    <main className="mt-20 py-[50px]">
+      <div className="container">
+        <BoardDetailCard
+          // TODO - api 작업 시 수정
+          isOwner={data.writer.id === 1}
+          title={data.title}
+          name={data.writer.name}
+          updatedAt={data.updatedAt}
+          likeCount={data.likeCount}
+          content={data.content}
+        />
+        {/* ANCHOR - 리뷰를 위해서 추가한 코드 */}
+        <BoardDetailCard
+          isOwner={data.writer.id === 2}
+          title={data.title}
+          name={data.writer.name}
+          updatedAt={data.updatedAt}
+          likeCount={data.likeCount}
+          content={data.content}
+        />
+      </div>
+    </main>
+  );
+}
