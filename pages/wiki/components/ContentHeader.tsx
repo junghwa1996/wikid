dad2a09ac2f7c3042f26f52b6071627f989976bf
김예도 <kyd629@naver.com>
2024-12-22 22:17:23 +0900

    ⚡️ SnackBar 수정사항 대응

--- pages/wiki/components/ContentHeader.tsx
+++ pages/wiki/components/ContentHeader.tsx
@@ -24,28 +24,86 @@ export default function ContentHeader({
   isEditing,
   isInfoSnackBarOpen,
 }: ContentHeaderProps) {
-  const [snackState, setSnackState] = useState<'success' | 'null'>('null');
+  const [linkSnackBarState, setLinkSnackBarState] = useState<{
+    open: boolean;
+    severity: 'fail' | 'success' | 'info';
+    message: string;
+    autoHideDuration?: number;
+  }>({
+    open: false,
+    severity: 'success',
+    message: '',
+  });
+
+  const [infoSnackBarState, setInfoSnackBarState] = useState<{
+    open: boolean;
+    severity: 'fail' | 'success' | 'info';
+    message: string;
+    autoHideDuration?: number;
+  }>({
+    open: true,
+    severity: 'info',
+    message: '앞 사람의 편집이 끝나면 위키 참여가 가능합니다.',
+    autoHideDuration: 300000,
+  });
 
   const handleLinkClick = () => {
     navigator.clipboard
       .writeText(link)
       .then(() => {
-        setSnackState('success');
-        setTimeout(() => setSnackState('null'), 1500);
+        setLinkSnackBarState({
+          open: true,
+          severity: 'success',
+          message: '클립보드에 복사되었습니다.',
+          autoHideDuration: 1500,
+        });
       })
       .catch(() => {
         alert('링크 복사에 실패했습니다.');
       });
   };
+
+  const handleCloseLinkSnackBar = () => {
+    setLinkSnackBarState({
+      ...linkSnackBarState,
+      open: false,
+    });
+  };
+
+  const handleCloseInfoSnackBar = () => {
+    setInfoSnackBarState({
+      ...infoSnackBarState,
+      open: false,
+    });
+  };
+
   return (
     <div>
       <div className="mb-[32px] flex items-center justify-between text-48sb text-gray-500 mo:mb-[24px] mo:text-32sb">
         {name}
       </div>
       {!isEditing && <LinkBar link={link} onClick={handleLinkClick} />}
-      {isInfoSnackBarOpen && <SnackBar state="info" />}
+      {isInfoSnackBarOpen && (
+        <SnackBar
+          severity={infoSnackBarState.severity}
+          open={infoSnackBarState.open}
+          onClose={handleCloseInfoSnackBar}
+          autoHideDuration={infoSnackBarState.autoHideDuration}
+        >
+          {infoSnackBarState.message}
+        </SnackBar>
+      )}
 
-      {snackState !== 'null' && <SnackBar state={snackState} />}
+      {
+        <SnackBar
+          severity={linkSnackBarState.severity}
+          open={linkSnackBarState.open}
+          onClose={handleCloseLinkSnackBar}
+          autoHideDuration={linkSnackBarState.autoHideDuration}
+        >
+          {linkSnackBarState.message}
+        </SnackBar>
+      }
     </div>
   );
 }
