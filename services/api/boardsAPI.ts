758ac71802e162ec7c1a7e309acee81e25c29d2c
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-23 19:21:31 +0900

    🔨 boards, userInfo 별도 api 요청 분리

---
+++ services/api/boardsAPI.ts
@@ -0,0 +1,40 @@
+import { ArticleData, BoardResponse, CommentsData } from 'types/board';
+
+import instance from '../../lib/axios-client';
+
+// 게시글 리스트 불러오기
+export const getBoards = async (query: string) => {
+  try {
+    const res = await instance.get(`/articles?${query}`);
+    return res.data as BoardResponse;
+  } catch {
+    throw new Error('게시글을 불러오지 못했습니다.');
+  }
+};
+
+// 게시글 상세 내용 불러오기
+export const getBoardDetail = async (
+  articleId: string
+): Promise<ArticleData> => {
+  try {
+    const res = await instance.get(`/articles/${articleId}`);
+    return res.data as ArticleData;
+  } catch {
+    throw new Error('게시글 데이터를 가져오는 중 문제가 발생했습니다.');
+  }
+};
+
+// 게시글 댓글 리스트 불러오기
+export const getComments = async (
+  articleId: string,
+  limit: number
+): Promise<CommentsData> => {
+  try {
+    const res = await instance.get(
+      `/articles/${articleId}/comments?limit=${limit}`
+    );
+    return res.data as CommentsData;
+  } catch {
+    throw new Error('댓글 데이터를 가져오는 중 문제가 발생했습니다.');
+  }
+};
