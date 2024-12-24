eaccf77af22b6b2455575eceb3033e3b5c195ea1
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-24 19:09:26 +0900

    🔨 viewport error 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,12 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
+      </Head>
       <ProfileProvider>
         <Head>
           <title>wikid</title>
