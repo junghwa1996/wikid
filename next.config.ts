48a95bb40deb4f68a6d9d62b1fc0e66a5d1dfd35
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-24 19:01:28 +0900

    🔨 / 경로 진입 시 /main으로 리다이렉션

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/main',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
