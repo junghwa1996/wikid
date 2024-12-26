6187b24c1dc1a3e5779ca765257fddadf5939ab7
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-26 17:08:40 +0900

    🔨 comment API 별도 분리

---
+++ services/api/commentAPI.ts
@@ -0,0 +1,35 @@
+import instance from '@/lib/axios-client';
+import { CommentsData } from 'types/board';
+
+// 댓글 페칭 함수
+export const fetchComments = async (
+  articleId: string | string[] | undefined,
+  cursor: string | null
+): Promise<CommentsData> => {
+  const response = await instance.get(
+    `/articles/${articleId}/comments?limit=10${cursor ? `&cursor=${cursor}` : ''}`
+  );
+  return response.data;
+};
+
+// 댓글 추가 함수
+export const addComment = async (articleId: string, content: string) => {
+  const response = await instance.post(`/articles/${articleId}/comments`, {
+    content,
+  });
+  return response.data;
+};
+
+// 댓글 수정 함수
+export const updateComment = async (id: number, newContent: string) => {
+  const response = await instance.patch(`/comments/${id}`, {
+    content: newContent,
+  });
+  return response.data;
+};
+
+// 댓글 삭제 함수
+export const deleteComment = async (id: number) => {
+  const response = await instance.delete(`/comments/${id}`);
+  return response.data;
+};
