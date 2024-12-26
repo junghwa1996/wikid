c9994028a6c5d57d0bf72718f1fb101911b2c607
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-26 20:00:05 +0900

    🔥 사용되지않는 import 삭제

--- pages/updateboard/[articleId].tsx
+++ pages/updateboard/[articleId].tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { FormEvent, useEffect, useState } from 'react';
