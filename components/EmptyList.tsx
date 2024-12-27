212eb0722a79e75e118230745dd83246abcfba32
jiminyee <jiminyee@crazyupinc.com>
2024-12-28 00:54:00 +0900

    🔧 이미지파일 형식 적용, 메인화면 레이아웃 수정

--- components/EmptyList.tsx
+++ components/EmptyList.tsx
@@ -15,7 +15,7 @@ export default function EmptyList({
     >
       <p className="text-20md text-gray-400 mo:text-18md">{text}</p>
       <Image
-        src="/images/empty.svg"
+        src="/images/empty.png"
         alt=""
         width={144}
         height={144}
