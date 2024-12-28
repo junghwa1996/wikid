e5eec537f12a81c0474c9c779853cbfbcff64424
tare <59001439+junghwa1996@users.noreply.github.com>
2024-12-28 23:10:02 +0900

    🔨 클라이언트 환경에서만 localstorage 사용

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 
 export const useAuth = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [accessToken, setAccessTokenState] = useState<string | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
   // accessToken 상태를 업데이트하면서 localStorage와 동기화
   const setAccessToken = (token: string | null) => {
@@ -14,12 +14,15 @@ export const useAuth = () => {
     setAccessTokenState(token);
   };
 
+  // 클라이언트 렌더링 후 accessToken 초기화
   useEffect(() => {
-    if (accessToken) {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
+    const token = localStorage.getItem('accessToken'); // 브라우저 환경에서만 localStorage 사용
+    setAccessTokenState(token);
+    setIsAuthenticated(!!token); // accessToken이 있으면 true로 설정
+  }, []);
+
+  useEffect(() => {
+    setIsAuthenticated(!!accessToken);
   }, [accessToken]);
 
   return {
