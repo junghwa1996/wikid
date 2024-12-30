5a3243b5e16096185825f77bcf1519421f44ae6d
김예도 <kyd629@naver.com>
2024-12-30 16:28:56 +0900

    🐛 타이틀 오타 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <ProfileProvider>
         <Head>
-          <title>wikid</title>
+          <title>wikied</title>
         </Head>
 
         <SnackbarProvider>
